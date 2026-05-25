c010bdecb3b0479b9f127169cca0525aad1f8f4c
pb-jwhitlock <peatedscotch@proton.me>
2026-05-24 21:19:31 -0400

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,1 @@
-export const formConfig = {
-  endpoint: "https://services.leadconnectorhq.com/hooks/kCwBYibHdMHe4b5OQqHh/webhook-trigger/c01a8798-360d-456e-b466-22864d404c9c",
-  method: "POST" as const,
-} as const;
+fix: add missing siteConfig export to unblock build
